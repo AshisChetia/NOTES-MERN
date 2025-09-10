cae87330d0f7e718c9b091f87231fc404ae01442
Ashis chetia <ashischetia9@gmail.com>
2025-09-10 21:29:58 +0530

    Backend Completed

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,20 +2,24 @@ import express from "express";
 import notesRoutes from "./Routes/notesRoutes.js";
 import {connectDb} from "./config/db.js";
 import dotenv from 'dotenv';
+import rateLimiter from "./middlewares/ratelimiter.js";
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
 
-connectDb()
 
-app.use(express.json())
+
+app.use(express.json()); //This middleware is purse JSON bodies: req.body
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
+connectDb().then(() => {
+  app.listen(PORT, () => {
   console.log("server is started on PORT:", PORT);
 });
+})
 
 
