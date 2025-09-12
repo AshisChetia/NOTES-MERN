5f4fbe74a49430ffc96b3d265fab3a5ade81534f
Ashis chetia <ashischetia9@gmail.com>
2025-09-12 22:12:53 +0530

    HomePage completed

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+
 import notesRoutes from "./Routes/notesRoutes.js";
 import {connectDb} from "./config/db.js";
-import dotenv from 'dotenv';
 import rateLimiter from "./middlewares/ratelimiter.js";
 
 dotenv.config()
@@ -10,9 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5001
 
 
-
-app.use(express.json()); //This middleware is purse JSON bodies: req.body
+//Middlewares
+app.use(cors({
+  origin: "http://localhost:5173",
+}))
 app.use(rateLimiter);
+app.use(express.json()); //This middleware is purse JSON bodies: req.body
+
 
 app.use("/api/notes", notesRoutes);
 
