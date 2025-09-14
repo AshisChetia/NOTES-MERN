9f3b21987c247398dab718639b096ba1cbb9cc63
Ashis chetia <ashischetia9@gmail.com>
2025-09-14 14:11:46 +0530

    Complete

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path'
 
 import notesRoutes from "./Routes/notesRoutes.js";
 import {connectDb} from "./config/db.js";
@@ -10,17 +11,30 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 
 //Middlewares
-app.use(cors({
-  origin: "http://localhost:5173",
-}))
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+    origin: "http://localhost:5173",
+  }))
+}
+
+
 app.use(rateLimiter);
 app.use(express.json()); //This middleware is purse JSON bodies: req.body
 
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
+}
 
 connectDb().then(() => {
   app.listen(PORT, () => {
